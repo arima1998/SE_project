47432000b41fb4109d57f834dbf0a6748893e499
Tawsif_Kz <arima1998@users.noreply.github.com>
2024-01-24 12:27:11 +0600

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
-import express from 'express';
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
 
-const app = express();
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"12345",
+    database:"test",
+    port:"3306",
+})
 
+const app=express();
 app.listen(3000,()=>{
     console.log('server running on port 3000!!');
 })
